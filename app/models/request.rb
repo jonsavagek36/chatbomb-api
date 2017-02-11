d35e1d8b2701106b1d4720c40deb5a6b1daa96e7
jonsavagek36 <jonsavagek36@gmail.com>
2017-02-11 15:20:02 -0500

    Added Models

---
+++ app/models/request.rb
@@ -0,0 +1,6 @@
+class Request < ApplicationRecord
+
+  belongs_to :req_sender, class_name: 'User', foreign_key: 'sender_id'
+  belongs_to :req_receiver, class_name: 'User', foreign_key: 'receiver_id'
+  
+end
