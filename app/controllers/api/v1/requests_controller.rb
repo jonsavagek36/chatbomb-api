9d686ad6710dc07cda7ff2aaa69ef612d15bb34e
jonsavagek36 <jonsavagek36@gmail.com>
2017-02-11 20:58:57 -0500

    Created send_request

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -12,4 +12,20 @@ class Api::V1::RequestsController < ApplicationController
     render json: { requests: @requesters }
   end
 
+  def send_request
+    data = JSON.parse(request.body.read)
+    @sender = User.find_by facebook_id: data["facebook_id"]
+    @receiver = User.find_by email: data["request_email"]
+    if @receiver.nil?
+      render json: { success: false, message: 'User not found.' }
+    else
+      @request = Request.create!(sender_id: @sender.id, receiver_id: @receiver.id)
+      if @request.save!
+        render json: { success: true, message: 'Request sent.' }
+      else
+        render json: { success: false, message: 'Error.' }
+      end
+    end
+  end
+
 end
