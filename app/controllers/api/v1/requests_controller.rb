defec5fa248280ca679f6c7c680ceee821a89c2c
jonsavagek36 <jonsavagek36@gmail.com>
2017-02-11 20:28:40 -0500

    Added get_requests

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -3,25 +3,13 @@ class Api::V1::RequestsController < ApplicationController
 
   def get_requests
     data = JSON.parse(request.body.read)
-    @user = User.find_by id: data["id"]
-    @request_ids = @user.received_requests
-    @requests = []
-    @request_ids.each do |request|
-      sender = Request.find(request).req_sender
-      @requests << User.find(sender)
+    @user = User.find_by facebook_id: data["facebook_id"]
+    @requests = @user.requests_received
+    @requesters = []
+    @requests.each do |request|
+      @requesters << User.find(request.sender_id)
     end
-    render json: @requests
-  end
-
-  def send_request
-    data = JSON.parse(request.body.read)
-    @receiver = User.find_by email: data["receiver_email"]
-    if @receiver.nil?
-      render json: { message: "User does not exist." }
-    else
-      Request.create!(req_sender: data["id"], req_receiver: @receiver.id)
-      render json: { message: "Request sent." }
-    end
+    render json: @requesters
   end
 
 end
