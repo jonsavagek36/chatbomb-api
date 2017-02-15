930206cf2d8e85940e6d4237894555eb19cd3350
jonsavagek36 <jonsavagek36@gmail.com>
2017-02-15 11:35:01 -0500

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
   has_many :friends, class_name: 'Friend', foreign_key: 'friend_id'
   has_many :chatters, class_name: 'Friend', foreign_key: 'user_id'
 
+  validates :facebook_id, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: true
+  validates :screen_name, uniqueness: true
 end
- 
